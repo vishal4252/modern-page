e5efac7ef9e3229511013d68f57772466a92553b
vishal <vp7390734@gmail.com>
2023-10-13 22:30:21 +0530

    modern landing page

---
+++ script.js
@@ -0,0 +1,53 @@
+Shery.imageEffect("#back", {
+  style: 5,
+  config: {
+    a: { value: 1.49, range: [0, 30] },
+    b: { value: -0.98, range: [-1, 1] },
+    aspect: { value: 2.14765097005531 },
+    gooey: { value: true },
+    infiniteGooey: { value: true },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.33, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: false },
+    onMouse: { value: 1 },
+    noise_speed: { value: 1.59, range: [0, 10] },
+    metaball: { value: 0.21, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0, range: [0, 0.1] },
+    noise_height: { value: 0.47, range: [0, 2] },
+    noise_scale: { value: 12.15, range: [0, 100] },
+  },
+  gooey: true,
+});
+
+const elems = document.querySelectorAll(".elem");
+elems.forEach((dets) => {
+  const headings = dets.querySelectorAll("h1");
+  let index = 0;
+  let flag = false;
+  document.querySelector("main").addEventListener("click", () => {
+    if (!flag) {
+      flag = true;
+      gsap.to(headings[index], {
+        top: "-=100%",
+        ease: Expo.easeInOut,
+        duration: 0.9,
+        onComplete: function () {
+          gsap.set(this._targets[0], { top: "100%" });
+          flag = false;
+        },
+      });
+      index === headings.length - 1 ? (index = 0) : index++;
+      gsap.to(headings[index], {
+        top: "-=100%",
+        ease: Expo.easeInOut,
+        duration: 1,
+      });
+    }
+  });
+});
